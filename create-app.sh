98cd92c2f27efd620bcadf07850faec123a7a010
simba-fs <simba.fs@gmail.com>
2020-09-07 21:11:38 +0800

    create-app.sh

--- create-app.sh
+++ create-app.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-appName='AURL'
 
-echo "$#"
-while($#); do
-	echo $1
-	case $1
-		-n|--name)
-				appName=$2
-				shift;shift;
-				;;
-		*|-h|--help)
-				echo cteate-app.sh [-n name]
-				shift;
-				;;
-	esac
-done
+if [[ -z $1 ]];then
+		appName=AURL
+else
+		appName=$1
+fi
+
+mkdir -p app/$1
+cd app/$1
+
+cp ../../docker-compose.yml .
+
 
 echo $appName
